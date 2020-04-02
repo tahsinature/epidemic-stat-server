1b1cc14752eb80d9feea39fba59261dfbc017fa9
Tahsin <tahsingenuies@gmail.com>
2020-04-02 22:41:33 +0700

    remove 500 error

--- src/controllers/stat.js
+++ src/controllers/stat.js
@@ -3,9 +3,6 @@ const covidSdk = require("../sdk/rapidCovid");
 
 module.exports = async (req, res) => {
   const countryName = req.query.country;
-  if (countryName === "US") return response.serverError(res);
   const data = await covidSdk.getResultByCountryName(countryName);
   response.success(res, data);
 };
-
-// covidSdk.getResultByCountryName("Italy");
