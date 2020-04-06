453f6697ef71ec25804430443030bbc94ce605e6
Tahsin <tahsingenuies@gmail.com>
2020-04-06 21:52:46 +0700

    - add github and google drive link for apk 0.4.4 - make minimum mobile requirement version 0.0.3 - latest mobile app version 0.4.4

--- src/controllers/manifest.js
+++ src/controllers/manifest.js
@@ -11,14 +11,18 @@ module.exports = async (req, res) => {
     isAppUpdateRequired: compareVersions.compare(envVars.MINIMUM_MOBILE_APP_REQUIREMENT, appVersion, ">"),
     latestAppVersion: envVars.LATEST_MOBILE_APP_VERSION,
     requiredAppVersion: envVars.MINIMUM_MOBILE_APP_REQUIREMENT,
-    appUpdateLinks: [{ playStore: "google.com", appStore: "apple.com" }],
+    appUpdateLinks: [{ playStore: "google.com", appStore: "apple.com" }], // unnecessary
     updateLinks: [
       // { source: "Play Store", sourceIcon: "https://cdn4.iconfinder.com/data/icons/free-colorful-icons/360/google_play.png", link: "https://play.google.com/store/apps/details?id=com.github.android" },
-      // { source: "Github", sourceIcon: "https://www.stickpng.com/assets/images/5847f98fcef1014c0b5e48c0.png", link: "https://github.com/clipto-pro/Desktop/releases/tag/v2.4.6" },
+      {
+        source: "Github",
+        sourceIcon: "https://www.stickpng.com/assets/images/5847f98fcef1014c0b5e48c0.png",
+        link: "https://github.com/tahsinature/epidemic-stat/releases/download/v0.0.4/Epidemic.Stat.0.0.4.apk",
+      },
       {
         source: "Google Drive",
         sourceIcon: "https://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Drive-icon.png",
-        link: "https://drive.google.com/open?id=1PsyKKnhbklYjOalH4VDTE4zkFyDsfFRL",
+        link: "https://drive.google.com/open?id=1Re2Z9WR6Eo9-95cFUbBlaMLMCl64TtZK",
       },
     ],
     meta: {
